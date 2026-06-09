d51fc5e45c25537a5c20fd74c6f709ff0c47b87d
Sarthak-67-cmd <sarthaksahassakhare@gmail.com>
2026-06-09 11:13:24 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,3 @@
-// firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
