986be3cf75207d802694dd653ee7cd2b5c4094a4
Sarthak-67-cmd <sarthaksahassakhare@gmail.com>
2026-06-09 07:55:43 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,30 @@
+import { auth } from "./firebase.js";
+
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+window.signup = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    alert("Account Created!");
+  } catch (e) {
+    alert(e.message);
+  }
+};
+
+window.login = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert("Login Successful!");
+  } catch (e) {
+    alert(e.message);
+  }
+};
