cccccd885033bf92077e994f03ba9bcab718f9e3
Sarthak-67-cmd <sarthaksahassakhare@gmail.com>
2026-06-09 10:28:24 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,14 +4,20 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
+  apiKey: "AIzaSyDbBGVunPw9baqJyc1NVCpa44IaENF_GQM",
   authDomain: "studybuddy-15aef.firebaseapp.com",
   projectId: "studybuddy-15aef",
   storageBucket: "studybuddy-15aef.firebasestorage.app",
   messagingSenderId: "686235862425",
-  appId: "1:686235862425:web:82bac01de4cc3929557f08"
+  appId: "1:686235862425:web:82bac01de4cc3929557f08",
+  measurementId: "G-P5FE41QW39"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+// Initialize Authentication
+const auth = getAuth(app);
+
+// Export auth
+export { auth };
